63ba2c0aa280aa275d011a43651e7afb683ed18b
joooi13 <joooi.y1313@gmail.com>
2022-05-28 17:32:30 +0900

    gha js test

--- js/sample.js
+++ js/sample.js
@@ -1,20 +1,5 @@
-function checkStatus(res) {
-    if (res.status >= 200 && res.status < 300) {
-        return res
-    } else {
-        const error = new Error(res.statusText);
-        error.response = res;
-        throw error
-    }
+
+function hello(name) {
+    return `Hello, ${name}!`;
 }
-
-function parseXML(response) {
-    return response.text().then((stringContainingXMLSource) => {
-        const parser = new DOMParser();
-        return parser.parseFromString(stringContainingXMLSource, "text/xml");
-    })
-};
-
-fetch("http://zip.cgis.biz/xml/zip.php?zn=0600000",{mode: 'cors'})
-.then(checkStatus).then(parseXML).catch((error) => {
-    console.log('request failed', error)});
+console.log(hello("World"));
